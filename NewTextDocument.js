834e75b95b087d8e53924295db2a7f7863fe1657
kuanle.niuace@gmail.com <lowkl-jm18@student.tarc.edu.my>
2022-09-10 16:56:26 +0800

    Prompt Added

--- NewTextDocument.js
+++ NewTextDocument.js
@@ -1,3 +1,6 @@
-function makeAlert() {
-    alert("This is a alert");
+function makeResponse(number) {
+    switch (number){
+        case 1: alert("This is a alert");
+        case 2: prompt("This is a prompt");
+    }
 }
